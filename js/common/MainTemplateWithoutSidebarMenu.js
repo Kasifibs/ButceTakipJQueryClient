d3b77b9554cede46861d788c0d2d6969ac6bb571
Kasifibs <dispinar01@gmail.com>
2016-12-19 00:17:12 +0300

    Login Sayfası düzenlendi

---
+++ js/common/MainTemplateWithoutSidebarMenu.js
@@ -0,0 +1,16 @@
+var MainTemplate = function(){
+	var that = this;
+
+	this.mainTemplateLoaded = function(doPageSpecificLoads){
+		$.get("/ButceTakip/views/common/NavBarToggle.html", function(data){
+		    $("#navPart").append(data);
+		});
+
+		$.get("/ButceTakip/views/common/NavBarTop.html", function(data){
+		    $("#navPart").append(data);
+		});
+
+		doPageSpecificLoads();
+	};
+
+}
