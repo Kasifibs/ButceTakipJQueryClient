1371579440af7817bf86cdd7491e8dd2dc935681
Kasifibs <dispinar01@gmail.com>
2017-02-24 19:55:37 +0300

    Varlık Kalemi sorgulama işlevi tamamlandı

--- js/itemoperations/resourceitem/ResourceItemQueryPanel.js
+++ js/itemoperations/resourceitem/ResourceItemQueryPanel.js
@@ -1,6 +1,8 @@
 var ResourceItemQueryPanel = function(){
 
   var that = this;
+  var resourceItemRetrieverObj = new ResourceItemRetriever();
+  var resourceItemTableGeneratorObj = new ResourceItemTableGenerator();
 
   this.resourceItemQueryPanelLoaded = function(){
     $('#hideResourceItemQueryPanelButton').click(that.hideResourceItemQueryPanel);
@@ -12,14 +14,16 @@ var ResourceItemQueryPanel = function(){
   this.queryButtonClicked = function(){
     var name = $('#resourceItemNameQueryTextField').val();
 
-
     var queryParams = {"name":name};
+
+    var resItemRetrieveHandlerOperation = resourceItemTableGeneratorObj.generateResourceItemTableFromResultData;
+    resourceItemRetrieverObj.retrieveUsingCriterias(queryParams, resItemRetrieveHandlerOperation);
   }
 
   this.cleanButtonClicked = function(){
     $('#resourceItemQueryDiv').empty();
 
-    $.get("/ButceTakip/views/itemoperations/resourceItem/ResourceItemQueryPanel.html", function(data){
+    $.get("/ButceTakip/views/itemoperations/resourceitem/ResourceItemQueryPanel.html", function(data){
         $("#resourceItemQueryDiv").append(data);
         that.resourceItemQueryPanelLoaded();
     });
